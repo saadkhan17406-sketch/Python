6bb6bfd57f773d7eed20d9e976d2243358b08bcd
saadkhan17406-sketch <saadkhan17406@eng.rizvi.edu.in>
2026-02-10 09:44:03 +0530

    Update EXP-10.py

--- EXP-10.py
+++ EXP-10.py
@@ -2,8 +2,15 @@
 #FACTORIAL GENERATOR
 print("UIN : 251A053  DATE : 10-02-2026")
 n = int(input("ENTER NUMBER : "))
+flag = False
 for i in range(2, n):
   if n%i == 0:
-    print(f"{i} IS NOT PRIME ")
+    Flag = True
+    break
   else:
-    print(f"{i} IS PRIME ")
+    Flag = False
+if Flag == True:
+  print("NOT PRIME")
+else:
+  print("PRIME")
+
