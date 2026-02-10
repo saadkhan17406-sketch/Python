050a10a3df1f6aec03f97c14dd770d31fec6b841
saadkhan17406-sketch <saadkhan17406@eng.rizvi.edu.in>
2026-02-10 09:11:10 +0530

    Add files via upload

---
+++ EXP-7.py
@@ -0,0 +1,9 @@
+#https://github.com/saadkhan17406-sketch/Python
+print("UIN : 251A053", "DATE : 09-02-2026")
+d={}
+n = int(input("ENTER NUMBER OF STUDENTS : "))
+for i in range(1,n+1):
+  Name = input("NAME : ")
+  Attendance = input("ATTENDANCE : ")
+  d[i] = {"NAME":Name, "ATTENDANCE":Attendance}
+print(d)
