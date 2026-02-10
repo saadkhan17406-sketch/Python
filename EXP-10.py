3c594e7bc78564fb0a50dc5828a95ca666e59ac7
saadkhan17406-sketch <saadkhan17406@eng.rizvi.edu.in>
2026-02-10 09:03:53 +0530

    Add files via upload

---
+++ EXP-10.py
@@ -0,0 +1,9 @@
+#https://github.com/saadkhan17406-sketch/Python
+#FACTORIAL GENERATOR
+print("UIN : 251A053  DATE : 10-02-2026")
+n = int(input("ENTER NUMBER : "))
+for i in range(2, n):
+  if n%i == 0:
+    print(f"{i} IS NOT PRIME ")
+  else:
+    print(f"{i} IS PRIME ")
